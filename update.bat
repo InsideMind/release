bcfa259d25ff5afd9d79486475a1e2c3ae00b3ab
Brett <me@brettalcorn.com>
2013-06-11 17:49:22 +1000

    next version

--- update.bat
+++ update.bat
@@ -1,7 +1,9 @@
-::@echo off
+@echo off
 
 cd %~dp0%
 set gitCmd="c:\Program Files (x86)\Git\bin\git.exe"
+:: Needed so git can find any ssh keys
+set HOME=%USERPROFILE%
 call %gitCmd% add --all
 call %gitCmd% commit -m "next version"
 call %gitCmd% push
