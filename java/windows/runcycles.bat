b5734bc76630495a84111cb15dfa1b91918ceae7
Brett <brett.alcorn@insidemind.com.au>
2013-06-25 13:03:41 +1000

    next version

--- java/windows/runcycles.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set tee="C:\program files (x86)\Git\bin\tee.exe"
-cd /d %~dp0%
-for /f %%i in (log/number);do set /a version=1+%%i
-set file=log/%version%.txt
-echo writing to %file%
-echo %version% >log/number
-cycles | %tee% %file%
